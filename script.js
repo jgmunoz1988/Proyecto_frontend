795719350209cc5e0a114beae008d1fd0aa7ea25
Jonathan Gabriel Muñoz <111930657+jgmunoz1988@users.noreply.github.com>
2022-09-28 00:47:20 -0300

    Agregado clase 9-10

---
+++ script.js
@@ -0,0 +1,78 @@
+/** Clase 10:  */
+
+/**console.log('hola mundo desde archivo externo');
+
+
+VARIABLES
+var nomb = "Harry";
+
+console.log(nomb);
+
+// STRING
+let nombre ="Harry";
+const NOMBRE = "Harry";
+
+console.log(nombre);
+
+
+// NUMERO
+let edad = 20 ;
+const NUM = 10.2;
+
+//BOOULEANO Significa: True o False
+
+
+
+let nombre = "Jonathan";
+
+let apellido = "Munoz";
+
+let edad = 33;
+
+let mascota = true;
+
+console.log(nombre)
+console.log(apellido)
+console.log(edad)
+console.log(mascota)
+
+//Operaciones
+
+/**let numeroA = 12;
+let numeroB = 3;
+
+let resultadoSuma = numeroA + numeroB;
+console.log(resultadoSuma);
+
+let resultadoMul = numeroA * numeroB;
+console.log(resultadoMul);
+
+let textoA = "Hola";
+let textoB = "Mundo";
+const BLANCO = " ";
+
+
+let resultadoA = textoA + BLANCO + textoB;
+
+console.log(resultadoA);
+
+
+// -----------> PROMT
+
+/**let entrada = prompt ("Ingrese su nombre");
+let salida = "Hola "+entrada;
+alert(salida);
+
+/**let numero = parseInt(prompt("Ingrese un numero"));
+const numeroB = 30;
+let salida = numero + numeroB;
+alert(salida); */                   
+
+
+
+
+
+
+
+
+
